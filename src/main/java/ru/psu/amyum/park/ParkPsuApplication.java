4f53406c6e4f840f136fe178741fc553fc7b775a
Антон Бардзак <49616233+enotu@users.noreply.github.com>
2025-03-19 23:57:50 +0500

    create template

---
+++ src/main/java/ru/psu/amyum/park/ParkPsuApplication.java
@@ -0,0 +1,13 @@
+package ru.psu.amyum.park;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParkPsuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParkPsuApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/psu/amyum/park/ParkPsuApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.psu.amyum.park;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParkPsuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
