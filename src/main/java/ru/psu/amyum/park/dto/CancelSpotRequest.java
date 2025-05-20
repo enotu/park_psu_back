d3dbdc1654028dc99016bdac9a9ff07576daa3c8
PalkinaSA <kseniashap0va98@gmail.com>
2025-05-20 20:21:59 +0500

    Liquibase: Make "cron.schedule" query

---
+++ src/main/java/ru/psu/amyum/park/dto/CancelSpotRequest.java
@@ -0,0 +1,22 @@
+package ru.psu.amyum.park.dto;
+
+public class CancelSpotRequest {
+    private Long spotId;
+    private Long parkingId;
+
+    public Long getSpotId() {
+        return spotId;
+    }
+
+    public void setSpotId(Long spotId) {
+        this.spotId = spotId;
+    }
+
+    public Long getParkingId() {
+        return parkingId;
+    }
+
+    public void setParkingId(Long parkingId) {
+        this.parkingId = parkingId;
+    }
+}
